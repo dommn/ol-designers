24747f947a6edb98296481cdee0aa849c520c4a2
cleavy10 <145566675+cleavy10@users.noreply.github.com>
2023-11-21 16:29:00 +0000

    More faffing

--- app/views/design-sandbox/support-form/_routes.js
+++
@@ -1,8 +0,0 @@
-const govukPrototypeKit = require('govuk-prototype-kit')
-const router = govukPrototypeKit.requests.setupRouter()
-
-// Add your routes here - above the module.exports line
-
-
-
-module.exports = router
